6c22072a2addcdb582fcfde3bd38e03c5896ee98
JeroMalt <144350349+JeroMalt@users.noreply.github.com>
2023-09-14 12:34:14 -0400

    created an alien

--- experiments/Activity1/js/script.js
+++ experiments/Activity1/js/script.js
@@ -21,7 +21,30 @@ function preload() {
 */
 function setup() {
     createCanvas(640,480);
+    
     background(160,40,75);
+    noStroke();
+
+    fill(0,150,0);  //body
+    ellipse(320, 480, 300, 200);
+
+    fill(10,200,10);  //head
+    ellipse(320, 240, 250, 360);
+    fill(10,200,10);
+    ellipse(320, 200, 300, 360);
+
+    fill(0,0,0);   //eyes 
+    ellipse(260, 180, 60, 150);
+    ellipse(380, 180, 60, 150);
+
+    ellipse(310, 280, 10, 10);   // nostrils
+    ellipse(330, 280, 10, 10);
+
+    stroke(200, 0, 0);
+    strokeWeight(10);
+    rectMode(CENTER);
+    rect(320, 340, 100, 20);
+    
 
 }
 
