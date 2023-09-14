ab76f9ec9b5cac73bdad5d0bbf7075c64fd86502
JeroMalt <144350349+JeroMalt@users.noreply.github.com>
2023-09-14 15:43:36 -0400

    finished up the activity

--- experiments/Activity3/js/script.js
+++ experiments/Activity3/js/script.js
@@ -26,7 +26,9 @@ let circle1 = {
     y : 250, 
     size : 100,
     fill : 255,
-    alpha : 200
+    alpha : 200,
+    speed : 1,
+    growth : 0.25
 
 }
 let circle2 = {
@@ -34,7 +36,8 @@ let circle2 = {
     y : 250, 
     size : 50,
     fill : 255,
-    alpha : 200
+    alpha : 200,
+    speed : -1
 }
 
 
@@ -55,12 +58,24 @@ function setup() {
 function draw() {
     //evolving red background
     background(bg.red, bg.green, bg.blue);
-    bg.red = bg.red + 1;
+    bg.red = map(circle1.size,0, 500, 0, 255);
+    
     //circle1
-    fill(circle1.fill);
+    fill(circle1.fill,[circle1.alpha]);
+    circle1.x = circle1.x + circle1.speed;//circle1 movement
+    circle1.x = constrain(circle1.x, 0,width / 2);
+
+    circle1.size = circle1.size + circle1.growth;
+    circle1.size = constrain(circle1.size, 0 , width );
     ellipse(circle1.x, circle1.y, circle1.size);
+
+    
     //circle2
-    fill(circle2.fill);
+    fill(circle2.fill,[circle2.alpha]);
+    circle2.x = circle2.x + circle2.speed; //circle2 movement
+    circle2.x = constrain(circle2.x,width / 2, 500);
+
+    circle2.size = circle1.size * 0.9;
     ellipse(circle2.x, circle2.y, circle2.size);
 
 }
