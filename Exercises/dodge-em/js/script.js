b993ee4916848f81abff976664e2be9da695b6e6
JeroMalt <144350349+JeroMalt@users.noreply.github.com>
2023-09-28 14:11:21 -0400

    started to right the code for A4

--- Exercises/dodge-em/js/script.js
+++ Exercises/dodge-em/js/script.js
@@ -1,9 +1,8 @@
 /**
- * Title of Project
- * Author Name
+ * DODGING COVID19
+ * Jérome Maltais
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * this is activity 4 
  */
 
 "use strict";
@@ -15,11 +14,26 @@ function preload() {
 
 }
 
-
+let covid19 = { //declaring the covid19 object/variable
+    x: 0,
+    y: 250,
+    size: 100,
+    vx: 0,
+    vy: 0,
+    speed: 500,
+    fill: {
+        r: 255,
+        g: 0,
+        b: 0
+    }
+};
 /**
  * Description of setup
 */
 function setup() {
+    createCanva(windowWidth, windowHeight);
+    covid19.y = random(0, height);
+    covid19.vx = covid19.speed; 
 
 }
 
@@ -28,5 +42,13 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(0);
+    
+    covid19.x = covid19.x + covid19.vx;
+    covid19.y = covid19.y + covid19.vy;
 
+    fill(covid19.fill.r, covid19.fill.g, covid19.fill.g);
+ 
+    circle(covid19.x, covid19.y, covid19.size);
+    
 }
