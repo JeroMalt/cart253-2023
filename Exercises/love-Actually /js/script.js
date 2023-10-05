c739c280cab0ad2f7ba99b4f047cd2f69dc9271b
JeroMalt <144350349+JeroMalt@users.noreply.github.com>
2023-10-05 14:55:03 -0400

    made circle move in a random direction appart from eachother

--- Exercises/love-Actually /js/script.js
+++ Exercises/love-Actually /js/script.js
@@ -1,32 +1,61 @@
 /**
- * Title of Project
- * Author Name
+ * Love Actually EX
+ * Jerome Maltais 
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * this is the code for the love actually exercise
  */
 
 "use strict";
 
-/**
- * Description of preload
-*/
+
 function preload() {
 
 }
 
+let circle1 = {
+    x : undefined,
+    y : 250,
+    size : 100,
+    vx : 0,
+    vy : 0, 
+    speed : 3
+};
+let circle2 = {
+    x : undefined,
+    y : 250,
+    size : 100,
+    vx : 0,
+    vy : 0, 
+    speed : 3
+};
 
-/**
- * Description of setup
-*/
 function setup() {
+    createCanvas(500,500);
+
+    //position circle separated form eachother 
+    circle1.x = width / 2;
+    circle2.x = 2 * width / 3;
+    
+    //start circle moving in random direction
+    circle1.vx = random(-circle1.speed, circle1.speed);
+    circle2.vx = random(-circle2.speed, circle2.speed);
+    circle1.vy = random(-circle1.speed, circle1.speed);
+    circle2.vy = random(-circle2.speed, circle2.speed);
 
 }
 
-
-/**
- * Description of draw()
-*/
 function draw() {
+    background(0);
+
+
+    circle1.x = circle1.x + circle1.vx; 
+    circle1.y = circle1.y + circle1.vy;
+
+    circle2.x = circle2.x + circle2.vx; 
+    circle2.y = circle2.y + circle2.vy;
+
+    ellipse(circle1.x, circle1.y, circle1.size);
+
+    ellipse(circle2.x, circle2.y, circle2.size);
 
 }
