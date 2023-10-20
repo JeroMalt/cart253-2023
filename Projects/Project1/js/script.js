d3012f26ad214d21dd4b830cf3183fe1f6479425
JeroMalt <144350349+JeroMalt@users.noreply.github.com>
2023-10-19 22:22:22 -0400

    added ship movement, secret ending

--- Projects/Project1/js/script.js
+++ Projects/Project1/js/script.js
@@ -15,6 +15,11 @@ let plan1Img
 let plan2Img
 let plan3Img
 let shipImg
+let shipDownImg
+let shipLeftImg
+let shipRightImg
+let titleImg
+let secretEnding
 
 function preload() {
     sunImg = loadImage('assets/images/sun.png');
@@ -22,11 +27,17 @@ function preload() {
     plan2Img = loadImage('assets/images/planet2.png');
     plan3Img = loadImage('assets/images/planet3.png');
     shipImg = loadImage('assets/images/ship.png');
+    shipDownImg = loadImage('assets/images/shipDown.png');
+    shipLeftImg = loadImage('assets/images/shipLeft.png');
+    shipRightImg = loadImage('assets/images/shipRight.png');
+    titleImg = loadImage('assets/images/title.png');
+    secretEnding = loadImage('assets/images/secret.png');
 }
 let ship = {
-    x : 0,
-    y : 0,
-    size : 32
+    x : 100,
+    y : 150,
+    size : 32,
+    speed : 10
 }   
 
 let sun = {
@@ -62,7 +73,10 @@ let planet3 = {
     
 let state = 'title';
 var deg = 10; //value for the rotation of the sun 
-
+let lookUp = 0;
+let lookDown = 180;
+let lookLeft = 90;
+let lookRight = 270;
 // the center of rotation for the planets:
 let centerX = 500;
 let centerY = 500;
@@ -104,43 +118,101 @@ function draw() {
         else if (state === 'planet3'){
             //event3
     }
-        else if (state === 'sun'){
+        else if (state === 'secret'){
             //sun event 
+            icarus();
         }
 
 }
 
+
 function title() {
-    push();
-    textSize(40);
-    fill(255);
-    textAlign(CENTER,CENTER);
-    text('clic mouse to start exploring', width/2, 750);
-    pop();
-    push();
-    textSize(75);
-    fill(255);
-    textAlign(CENTER,CENTER);
-    text('SPACE EXPLORER', width/2, 400);
-    pop();
+
+    imageMode(CENTER);
+    image(titleImg,500,500,1000,1000);
+    
+    //old title function
+    // push();
+    // textSize(40);
+    // fill(255);
+    // textAlign(CENTER,CENTER);
+    // text('clic mouse to start exploring', width/2, 750);
+    // pop();
+    // push();
+    // textSize(75);
+    // fill(255);
+    // textAlign(CENTER,CENTER);
+    // text('SPACE EXPLORER', width/2, 400);
+    // pop();
 
 }
+function icarus() {
+    //background(0);
+    imageMode(CENTER);
+    image(secretEnding, 500,500,1000,1000);
+    } 
 function exploration(){
-  image(shipImg, mouseX,mouseY,32,32);
-    
-  //ellipse(mouseX,mouseY,50,50)
-
+  
+  shipMovement();
   sunrotation();
   planetuno();
- //checkOverlap();
+  
+  //setVolume(0.1);
+  //spaceSound.play();
+ checkOverlap();
 
 
+}
+function shipMovement(){ //display ship and control ship+sound n stuff
+    
+    imageMode(CENTER);
+    
+    if(keyIsDown(UP_ARROW)){ 
+        ship.y = ship.y - ship.speed; 
+        image(shipImg, ship.x, ship.y ,ship.size, ship.size);
+        //shipSound.play();
+        if(ship.y < 0){
+            ship.y = 1000;
+        }
+
+    }
+    else if(keyIsDown(DOWN_ARROW)){ 
+        ship.y = ship.y + ship.speed; 
+        image(shipDownImg, ship.x, ship.y ,ship.size, ship.size);
+        //shipSound.play();
+        if(ship.y > 1000){
+            ship.y = 0;
+        }
+
+        
+    }
+    else if(keyIsDown(LEFT_ARROW)){
+        ship.x = ship.x - ship.speed;
+        image(shipLeftImg, ship.x, ship.y ,ship.size, ship.size);
+        //shipSound.play();
+        if(ship.x < 0){
+            ship.x = 1000;
+        }
+    }
+    else if(keyIsDown(RIGHT_ARROW)){
+        ship.x = ship.x + ship.speed;
+        image(shipRightImg, ship.x, ship.y ,ship.size, ship.size);
+        //shipSound.play();
+        if(ship.x > 1000){
+            ship.x = 0;
+        }
+    }
+    else {
+        image(shipImg, ship.x, ship.y ,ship.size, ship.size);
+        //shipSound.stop();
+    }
+    
+    
 }
 function sunrotation(){ //display and rotation of the sun
   push(); 
   translate (width/2,height/2);
   imageMode (CENTER);
-  fill(255,165,50);
   rotate (radians (deg));
   image(sunImg, sun.x, sun.y, sun.size,sun.size);
  // ellipse(sun.x,sun.y,sun.size);
@@ -177,14 +249,14 @@ function planetuno(){ //function displays and makes the planets rotate around th
   planet3.angle = planet3.angle + planet3.speed;
 }
 
-// function checkOverlap() {
-//     //check if the flshdrive and the port overlap (connect)
-//     let d = dist(mouseX,mouseY,sun.x,sun.y);
-//     if (d < circle1.size/3 + circle2.size/3){
-//         state = 'love';
-//     }
+function checkOverlap() {
+    //check if the flshdrive and the port overlap (connect)
+    let d = dist(ship.x,ship.y,500,500);
+    if (d < ship.size/2 + sun.size/4){
+        state = 'secret';
+    }
 
-// }
+}
 
 function mousePressed(){
     if (state === 'title'){
