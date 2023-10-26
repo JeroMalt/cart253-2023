80f20c1cedfddda48dec35dbf2a3de060f93fb47
JeroMalt <144350349+JeroMalt@users.noreply.github.com>
2023-10-26 18:05:32 -0400

    added the user

--- Exercises/age-of-aquariums/js/script.js
+++ Exercises/age-of-aquariums/js/script.js
@@ -11,11 +11,21 @@
 function preload() {
 
 }
+
+let circle = {
+    x: 0,
+    y: 0,
+    size: 50,
+    trail: [], // Note that we are creating an EMPTY TRAIL ARRAY as a PROPERTY of the circle
+    wormSize : 20
+  }
+let state = 'title'; 
 let school = [];
 let schoolSize = 6;
 
 
 function setup() {
+    frameRate(30);
     createCanvas(600,600);
 
     for(let i = 0; i < schoolSize; i ++) {
@@ -37,18 +47,101 @@ function createFish(x, y) {
     return fish;
 }
 
-/**
- * Description of draw()
-*/
 function draw() {
     background(0);
-
-    for(let i = 0; i < school.length; i ++) {
-      moveFish(school[i]);
-      displayFish(school[i]);
-    }
+    mainFrame();
+    timer();
+   
+    
 }
 
+function mainFrame(){
+    if (state === 'title') {
+        title();
+
+    }
+    else if (state === 'simulation'){
+        simulation();
+    }
+    else if (state === 'end1'){
+        end1();
+    }
+    else if (state === 'end2'){
+        end2();
+    }
+}
+function simulation(){
+    for(let i = 0; i < school.length; i ++) {
+        moveFish(school[i]);
+        displayFish(school[i]);
+      }
+      timeDisplay();
+      worm();
+}
+function title(){
+    background(0);
+
+}
+function end1(){
+    push();
+    textSize(50);
+    textAlign(CENTER);
+    fill(255);
+    text('LOSER!', width/2, height/3);
+    pop();
+    
+
+}
+function end2(){
+
+}
+
+
+function worm(){
+    for (let i = 0; i < circle.trail.length; i++) {
+        // Get the element at the index indicated by i (0, then 1, then 2, etc.)
+        let element = circle.trail[i];
+        // Draw an ellipse the same size as the circle at that position
+        fill(255);
+        ellipse(element.x, element.y, circle.size);
+      }
+    
+      // Move the circle to the mouse position
+      circle.x = mouseX;
+      circle.y = mouseY;
+    
+      // Draw the circle
+      ellipse(circle.x, circle.y, circle.size);
+    
+      // Create a new position object that stores where the circle is now
+      // which we can add to the trail to trace the path of the circle
+      let newTrailPosition = {
+        x: circle.x,
+        y: circle.y
+      };
+      // Add the position to the circle's trail array
+      circle.trail.push(newTrailPosition);
+      if(circle.trail.length > circle.wormSize) {
+        circle.trail.shift();
+      }
+    }
+
+function timer(){
+    if (frameCount >= 900) {
+        state = 'end1';
+
+    }
+}
+function timeDisplay(){
+    let countDownRaw = map(frameCount,0 , 900, 30, 0);
+    let countDownCooked = round(countDownRaw, 1);
+    push();
+    textSize(30);
+    textAlign(CENTER);
+    fill(255);
+    text(countDownCooked, width/2, height/6);
+    pop();
+}
 function moveFish(fish){
     let change = random(0,1);
     if(change < 0.05) {
@@ -63,6 +156,7 @@ function moveFish(fish){
     fish.y = constrain(fish.y, 0, height);
 
 }
+
 function displayFish(fish) {
     push();
     fill(200,100,100);
@@ -70,7 +164,15 @@ function displayFish(fish) {
     ellipse(fish.x, fish.y, fish.size);
     pop();
 }
+
 function mousePressed() {
     let fish = createFish(mouseX, mouseY);
     school.push(fish);
+}
+function keyPressed(){
+    if (state === 'title'){
+        state = 'simulation';
+        //starts the simulation
+    }
+    
 }
