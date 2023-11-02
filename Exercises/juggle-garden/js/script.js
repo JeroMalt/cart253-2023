a85e252cb203783e0c38d5026ed7e8950504f1df
JeroMalt <144350349+JeroMalt@users.noreply.github.com>
2023-11-02 11:34:48 -0400

    started the ex

--- Exercises/juggle-garden/js/script.js
+++ Exercises/juggle-garden/js/script.js
@@ -8,25 +8,44 @@
 
 "use strict";
 
-/**
- * Description of preload
-*/
+
 function preload() {
 
 }
 
+let paddle;
+let gravityForce = 0.0025;
+let balls = [];
+let numBalls = 3;
 
-/**
- * Description of setup
-*/
 function setup() {
+    createCanvas(windowWidth,windowHeight);
+
+    paddle = new Paddle(300,20);
+
+    for (let i = 0; i < numBalls; i ++){
+        let x = random(0,width);
+        let y = random(-400,-100);
+        let ball = new Ball(x,y);
+        balls.push(ball);
+    }
 
 }
 
 
-/**
- * Description of draw()
-*/
+
 function draw() {
+    background(0);
+
+    paddle.move();
+    paddle.display();
+
+    for (let i =0; i < balls.length; i++) {
+        let ball = balls[i];
+        ball.gravity(gravityForce);
+        ball.move();
+        ball.bounce();
+        ball.display();
+    }
 
 }
