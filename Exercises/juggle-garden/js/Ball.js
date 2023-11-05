d1688bdb20f5dfbb743ec97ebc09eb0c720d1ab9
JeroMalt <144350349+JeroMalt@users.noreply.github.com>
2023-11-05 16:15:19 -0500

    finish the ex

--- Exercises/juggle-garden/js/Ball.js
+++ Exercises/juggle-garden/js/Ball.js
@@ -25,13 +25,26 @@ move() {
     
     this.x = this.x + this.vx;
     this.y = this.y + this.vy;
+
+    if (this.y - this.size/2 > height) {
+        this.active = false;
+    }
 }
 
-bounce() {
-    if(this.y + this.size/2 >= height) {
-        this.vy = this.vy;
+bounce(paddle) {
+    if (this.x > paddle.x - paddle.width/2 && 
+        this.x < paddle.x + paddle.width/2 &&
+        this.y + this.size/2 > paddle.y - paddle.height/2 &&
+        this.y - this.size/2 < paddle.y + paddle.height/2) {
+        
+        let dx = this.x - paddle.x;
+        this.vx = this.vx + map(dx, -paddle.width/2,paddle.width/2,-2,2);
+
+        
+        this.vy = -this.vy;
         this.ay = 0;
     }
+       
 }
 display(){
     push();
