227fe4c375a6b58440b07bbd4802ef9b1b3786d3
JeroMalt <144350349+JeroMalt@users.noreply.github.com>
2023-11-17 09:59:14 -0500

    finished the sound activity

--- experiments/SoundActivity/js/script.js
+++ experiments/SoundActivity/js/script.js
@@ -1,10 +1,8 @@
 /**
- * Title of Project
- * Author Name
+ * sound Activty
+ * Jerome Maltais
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
- */
+ * 
 
 "use strict";
 
@@ -14,12 +12,18 @@
 function preload() {
 
 }
+let balls = [];
 
+//f-minor
 
+let notes = ['F3','G3','Ab4','Bb4','C4','Db4','Eb4','F4'];
 /**
  * Description of setup
-*/
+*/ 
 function setup() {
+    createCanvas(600,600);
+
+    userStartAudio();
 
 }
 
@@ -28,5 +32,23 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(0);
+    
+    for (let i = 0; i < balls.length; i ++) {
+        let ball = balls[i];
+        ball.move();
+        ball.bounce();
+        ball.display();
+    }
+
+}
+function mousePressed(){
+    createBall(mouseX,mouseY,);
+}
+
+function createBall(x,y) {
+    let note = random(notes);
+    let ball = new Ball(x,y,note);
+    balls.push(ball);
 
 }
