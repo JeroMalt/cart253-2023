70d4c710a6f8f38a8b9ae5894790a9557b6a916b
JeroMalt <144350349+JeroMalt@users.noreply.github.com>
2023-12-08 22:59:55 -0500

    project done

--- Projects/Project2/prototype3/js/Ball.js
+++ Projects/Project2/prototype3/js/Ball.js
@@ -1,105 +1,74 @@
+// Class representing a bouncing ball
 class Ball {
-
-constructor(x,y) { 
-    this.x = x;
-    this.y = y;
-    this.vx = 0;
-    this.vy = 0;
-    this.ax = 0; 
-    this.ay = 0;
-    this.maxSpeed = 10;
-    this.size = 100;
-    this.active = true;
-    this.bc = false;
-    this.fail = true;
-
-}
-
-gravity(force) {
-    this.ay = this.ay + force;
-}
-
-move() {
-    this.fail = false;
-    this.vx = this.vx + this.ax;
-    this.vy = this.vy + this.ay;
-    
-    this.vx = constrain(this.vx, -this.maxSpeed, this.maxSpeed);
-    this.vy = constrain(this.vy, -this.maxSpeed, this.maxSpeed);
-    
-    this.x = this.x + this.vx;
-    this.y = this.y + this.vy;
-
-    if (this.y - this.size/2 > height) {
-        this.active = false;
-        this.fail = true;
-    }
-    
-    
-    
-}
-
-bounce(paddle) {
-    if (this.x > paddle.x - paddle.width/2 && 
-        this.x < paddle.x + paddle.width/2 &&
-        this.y + this.size/2 > paddle.y - paddle.height/2 &&
-        this.y - this.size/2 < paddle.y + paddle.height/2) {
-        
-        let dx = this.x - paddle.x;
-        this.vx = this.vx + map(dx, -paddle.width/2,paddle.width/2,-2,2);
-
-        this.bc = this.bc + 1;
-        this.vy = -this.vy;
-        this.ay = 0;
-        this.bc = true;
-        
-        
-        
-    }
-    else {
-        this.bc = false;
+    // Constructor to initialize ball properties
+    constructor(x, y) {
+        this.x = x;               // X-coordinate of the ball
+        this.y = y;               // Y-coordinate of the ball
+        this.vx = 0;              // X-component of velocity
+        this.vy = 0;              // Y-component of velocity
+        this.ax = 0;              // X-component of acceleration
+        this.ay = 0;              // Y-component of acceleration
+        this.maxSpeed = 10;       // Maximum speed of the ball
+        this.size = 100;          // Size of the ball
+        this.active = true;       // Flag to determine if the ball is active
+        this.bc = false;          // Flag to indicate if the ball bounced
+        this.fail = true;         // Flag to indicate if the ball has failed
     }
 
-    
-    
-    
-   
+    // Apply gravity to the ball
+    gravity(force) {
+        this.ay = this.ay + force;
+    }
 
-    
+    // Move the ball based on velocity and acceleration
+    move() {
+        this.fail = false;
+        this.vx = this.vx + this.ax;
+        this.vy = this.vy + this.ay;
 
-    
-       
-}
+        // Constrain velocity to avoid excessive speed
+        this.vx = constrain(this.vx, -this.maxSpeed, this.maxSpeed);
+        this.vy = constrain(this.vy, -this.maxSpeed, this.maxSpeed);
 
-// displayBounce() {
-//     push();
-//     fill(255);
-//     stroke(20);
-//     textSize(100);
-//     text(this.bc,100,200);
-//     pop();
-// }
-// displayHighscore() {
-//     if (this.hs < this.bc) {
-//         this.hs = this.bc;
-//     }
-//     push();
-//     fill(255);
-//     stroke(20);
-//     textSize(100);
-//     text(this.hs,800,200);
-//     pop();
-// }
+        this.x = this.x + this.vx;
+        this.y = this.y + this.vy;
 
+        // Deactivate the ball if it goes below the screen
+        if (this.y - this.size / 2 > height) {
+            this.active = false;
+            this.fail = true;
+        }
+    }
 
+    // Check for collision with a paddle and update ball properties
+    bounce(paddle) {
+        if (
+            this.x > paddle.x - paddle.width / 2 &&
+            this.x < paddle.x + paddle.width / 2 &&
+            this.y + this.size / 2 > paddle.y - paddle.height / 2 &&
+            this.y - this.size / 2 < paddle.y + paddle.height / 2
+        ) {
+            let dx = this.x - paddle.x;
+            this.vx = this.vx + map(dx, -paddle.width / 2, paddle.width / 2, -2, 2);
 
+            this.bc = this.bc + 1;
+            this.vy = -this.vy;
+            this.ay = 0;
+            this.bc = true;
+        } else {
+            this.bc = false;
+        }
+    }
 
-display() {
-    image(img, this.x,this.y,this.size,this.size); 
-    // push();
-    // fill(255,50,50);
-    // stroke(0);
-    // ellipse(this.x,this.y,this.size);
-    // pop();
-}
+    // Display the ball on the canvas
+    display() {
+        // Display the ball image at its current position
+        image(img, this.x, this.y, this.size, this.size);
+        // Alternatively, display the ball as an ellipse
+        // push();
+        // fill(255, 50, 50);
+        // stroke(0);
+        // ellipse(this.x, this.y, this.size);
+        // pop();
+    }
 }
--- Projects/Project2/prototype3/js/script.js
+++ Projects/Project2/prototype3/js/script.js
@@ -1,13 +1,28 @@
+//HEADER SIMULATOR 
+//by jerome Maltais 
+//fall 2023
+
+//made with the jungle-garden exercise 
+//also using the blazeFace model with help form this tutorial https://www.youtube.com/watch?v=jovusqHNpRo&t=25s 
 
 
+// Declare global variables for images
 let img;
 let img2;
+let img3;
+let img4;
+let img5;
 
-
+// Preload function to load images before setup
 function preload() {
-img = loadImage('assets/images/soccerBall.png');
-img2 = loadImage('assets/images/field.jpg');
+  img = loadImage('assets/images/soccerBall.png');
+  img2 = loadImage('assets/images/field.jpg');
+  img3 = loadImage('assets/images/title.png');
+  img4 = loadImage('assets/images/fail.png');
+  img5 = loadImage('assets/images/win.png');
 }
+
+// Declare global variables for video, face model, paddle, and other parameters
 let video;
 let model;
 let face;
@@ -15,217 +30,276 @@ let paddle;
 let gravityForce = 0.0095;
 let balls = [];
 let numBalls = 1;
-let bounceScore = 0; 
+let bounceScore = 0;
 let highScore = 0;
+let state = 'title';
+let synth
+let difficulty = 5;
 
-
+// Setup function to initialize canvas, video, and objects
 function setup() {
-    createCanvas(1028,720);
-    img = loadImage('assets/images/soccerBall.png');
+  createCanvas(1028, 720);
 
-    video = createCapture(VIDEO);
-    video.hide();
+  // Capture video from the webcam
+  video = createCapture(VIDEO);
+  video.hide();
 
-    loadFaceModel();
+  // Initialize a Monophonic Synthesizer
+  synth = new p5.MonoSynth();
 
-    //create the paddle
-    //paddle = new Paddle(300,20);
-//creates the balls
-    for (let i = 0; i < numBalls; i ++){
-        let x = random(0,width);
-        let y = random(-400,-100);
-        let ball = new Ball(x,y);
-        balls.push(ball);
-    }
+  // Load the BlazeFace model for face detection
+  loadFaceModel();
 
+  // Create the initial set of balls
+  for (let i = 0; i < numBalls; i++) {
+    let x = random(0, width);
+    let y = random(-400, -100);
+    let ball = new Ball(x, y);
+    balls.push(ball);
+  }
 }
+
+// Asynchronous function to load the BlazeFace model
 async function loadFaceModel() {
-    model = await blazeface.load();
+  model = await blazeface.load();
 }
 
+// Main draw function, calls different states of the game
 function draw() {
-   
-    
-    
-    translate(width, 0);
-    //then scale it by -1 in the x-axis
-    //to flip the image
-    scale(-1, 1);
-    image(img2, width/2,height/2,width,height);
+  mainFrame();
+}
 
+// Function to manage different states of the game
+function mainFrame() {
+  if (state === 'title') {
+    title();
+  } else if (state === 'header') {
+    header();
+  } else if (state === 'fail') {
+    fail();
+  } else if (state === 'win') {
+    win();
+  }
+}
+
+// Display the title screen
+function title() {
+  imageMode(CENTER);
+  image(img3, width / 2, height / 2, width, height);
+}
+function header() {
+    // Translate to the right edge of the canvas and flip it horizontally
+    translate(width, 0);
+    scale(-1, 1);
+
+    // Check if video metadata is loaded and the face model is defined
     if (video.loadedmetadata && model !== undefined) {
+        // Get the face data from the webcam
         getFace();
     }
 
-         if (face !== undefined) {
+    // Check if a face is detected
+    if (face !== undefined) {
+        // Display the background image
+        imageMode(CENTER);
+        image(img2, width / 2, height / 2);
 
-            
-
-
-          // background(255);
-          imageMode(CENTER);
-          image(img2, width/2,height/2);
-
-
+        // Initialize variables for facial features and dimensions
         let bodyx = 0;
         let bodyy = 0;
 
         let rightEye = face.landmarks[0];
-        let leftEye =  face.landmarks[1];
-        let nose =     face.landmarks[2];
+        let leftEye = face.landmarks[1];
+        let nose = face.landmarks[2];
         let rightEar = face.landmarks[4];
-        let leftEar =  face.landmarks[5];
+        let leftEar = face.landmarks[5];
 
+        // Scale facial features to match canvas dimensions
         rightEye = scalePoint(rightEye);
-        leftEye =  scalePoint(leftEye);
-        nose =     scalePoint(nose);
+        leftEye = scalePoint(leftEye);
+        nose = scalePoint(nose);
         rightEar = scalePoint(rightEar);
         leftEar = scalePoint(leftEar);
 
-
-        let faceSize = dist(rightEar.x, rightEar.y,leftEar.x,leftEar.y);
-        let eyeSize = dist(rightEye.x, rightEye.y,leftEye.x,leftEye.y);
-        //let noseSize = 
-        let faceCenterx = (leftEar.x + rightEar.x + nose.x + rightEye.x + leftEye.x)/5;
-        let faceCentery = (leftEar.y + rightEar.y + nose.y + rightEye.y + leftEye.y)/5;
+        // Calculate face and eye dimensions, and the position of facial features
+        let faceSize = dist(rightEar.x, rightEar.y, leftEar.x, leftEar.y);
+        let eyeSize = dist(rightEye.x, rightEye.y, leftEye.x, leftEye.y);
+        let faceCenterx = (leftEar.x + rightEar.x + nose.x + rightEye.x + leftEye.x) / 5;
+        let faceCentery = (leftEar.y + rightEar.y + nose.y + rightEye.y + leftEye.y) / 5;
         bodyx = faceCenterx;
-        bodyy = faceCentery *1.2;
+        bodyy = faceCentery * 1.2;
 
-
-         //body
-         push();
-         fill(255,0,0);
-         triangle(bodyx, bodyy, bodyx/2, bodyy*3,bodyx*1.5, bodyy*3);
-         pop();
- 
- 
-        //head
+        // Draw the body as a triangle
         push();
-        rectMode(CENTER);
-        fill(255,192,203);
-        rect(faceCenterx, faceCentery ,faceSize,faceSize * 1.5, 20);
+        fill(255, 0, 0);
+        triangle(bodyx, bodyy, bodyx / 2, bodyy * 3, bodyx * 1.5, bodyy * 3);
         pop();
 
-        //eyes
+        // Draw the head as a rectangle
+        push();
+        rectMode(CENTER);
+        fill(255, 192, 203);
+        rect(faceCenterx, faceCentery, faceSize, faceSize * 1.5, 20);
+        pop();
+
+        // Draw the eyes
         push();
         ellipseMode(CENTER);
         fill(255);
-        ellipse(rightEye.x, rightEye.y,eyeSize/2,eyeSize/2 * 1.5);
-        ellipse(leftEye.x, leftEye.y,eyeSize/2,eyeSize/2 * 1.5);
+        ellipse(rightEye.x, rightEye.y, eyeSize / 2, eyeSize / 2 * 1.5);
+        ellipse(leftEye.x, leftEye.y, eyeSize / 2, eyeSize / 2 * 1.5);
         pop();
-        
+
+        // Draw the pupils
         push();
         ellipseMode(CENTER);
         fill(0);
-        ellipse(rightEye.x, rightEye.y,eyeSize/4);
-        ellipse(leftEye.x, leftEye.y,eyeSize/4);
+        ellipse(rightEye.x, rightEye.y, eyeSize / 4);
+        ellipse(leftEye.x, leftEye.y, eyeSize / 4);
         pop();
-        
-        //nose
-        // push();
-        // rectMode(CENTER);
-        // fill(150,75,100);
-        // rect(nose.x, nose.y,25,40);
-        // pop(); 
 
-        //mouth
+        // Draw the nose
+        push();
+        rectMode(CENTER);
+        fill(150, 75, 100);
+        rect(nose.x, nose.y, eyeSize / 2.5, eyeSize * 0.7);
+        pop();
+
+        // Draw the mouth as a line
         push();
         stroke(0);
         strokeWeight(10);
-        line(rightEye.x, rightEye.y*1.3, leftEye.x,leftEye.y*1.3);
+        line(rightEye.x, rightEye.y * 1.3, leftEye.x, leftEye.y * 1.3);
         pop();
-      
-        //nose
-        push();
-        rectMode(CENTER);
-        fill(150,75,100);
-        //rect(nose.x, nose.y,25,40);
-        rect(nose.x, nose.y,eyeSize/2.5,eyeSize*0.7);
-        pop(); 
-       paddle = new Paddle(faceSize, 0,faceCenterx,faceCentery-(faceSize/1.5));
-        //paddle.move();
-        //paddle.display();
 
-        for (let i =0; i < balls.length; i++) {
+        // Create the paddle based on face dimensions
+        paddle = new Paddle(faceSize, 0, faceCenterx, faceCentery - (faceSize / 1.5));
+
+        // Iterate through each ball and update its position, check for collisions, and display
+        for (let i = 0; i < balls.length; i++) {
             let ball = balls[i];
-            if (ball.active){
+            if (ball.active) {
                 ball.gravity(gravityForce);
                 ball.move();
                 ball.bounce(paddle);
                 ball.display();
-                if (ball.y - ball.size/2 > height) {
-                    bounceScore = bounceScore -1;
-
+                if (ball.y - ball.size / 2 > height) {
+                    bounceScore = bounceScore - 1;
                 }
-                
-                
-
-                
             }
-            
-           
-            
-            if (ball.bc){
-                bounceScore = bounceScore + 1;
+            if (ball.bc) {
+                bounceScore = bounceScore + difficulty;
+                playSynth();
             }
-            
-            
-            
-            
-           
-
         }
+
+        // Update and display scores, check for game over or win conditions
         if (bounceScore > highScore) {
             highScore = bounceScore;
         }
         displayScore();
 
-    
+        if (bounceScore <= -15) {
+            state = 'fail';
+        }
+
+        if (bounceScore >= 50) {
+            state = 'win';
+        }
     }
 }
 
-function displayScore(){
-    push();
-    translate(width, 0);
-    scale(-1, 1);
+
+// Display the fail screen with the current high score
+function fail() {
+    // Set image mode to center and display the fail image
+    imageMode(CENTER);
+    image(img4, width / 2, height / 2, width, height);
+
+    // Display the high score with text settings
+    textAlign(CENTER);
     fill(255);
     stroke(20);
     textSize(100);
-    text(bounceScore,100,200);
-    text(highScore,600,200);
-    pop();
-
+    text(highScore, (width / 2) + 300, 200);
 }
 
+// Display the win screen
+function win() {
+    // Set image mode to center and display the win image
+    imageMode(CENTER);
+    image(img5, width / 2, height / 2, width, height);
+}
+
+// Display the current and high scores
+function displayScore() {
+    // Flip the score display horizontally
+    push();
+    translate(width, 0);
+    scale(-1, 1);
+
+    // Set text settings and display current and high scores
+    textAlign(CENTER);
+    fill(255);
+    stroke(20);
+    textSize(100);
+    text(bounceScore, (width / 2) - 300, 200);
+    text(highScore, (width / 2) + 300, 200);
+    pop();
+}
+
+// Scale a point from video coordinates to canvas coordinates
 function scalePoint(pt) {
     let x = map(pt[0], 0, video.width, 0, width);
     let y = map(pt[1], 0, video.height, 0, height);
-    return createVector(x,y);
+    return createVector(x, y);
 }
-async function getFace() {
 
-    const predictions = await model.estimateFaces(
-        document.querySelector('video'),
-        false
-    );
+// Asynchronous function to get the face from the webcam using BlazeFace
+async function getFace() {
+    const predictions = await model.estimateFaces(document.querySelector('video'), false);
 
     if (predictions.length === 0) {
         face = undefined;
-    }
-    else {
+    } else {
         face = predictions[0];
     }
-    
 }
 
+// Handle typed keys, create a new ball on 'r'
 function keyTyped() {
-    if (key === 'r'){
-        let x = random(0,width);
-        let y = random(-400,-100);
-        let ball = new Ball(x,y);
+    if (key === 'r') {
+        // Create a new ball at a random position
+        let x = random(10, width - 10);
+        let y = random(-400, -100);
+        let ball = new Ball(x, y);
         balls.push(ball);
-        }
-        //return false;
+    }
+}
+
+// Handle key press, change state from title to header
+function keyPressed() {
+    if (state === 'title') {
+        state = 'header';
+        // Start the simulation
+    }
+}
+
+// Play a synthesizer note when a ball bounces
+function playSynth() {
+    userStartAudio();
+
+    // Choose a random note for the synthesizer
+    let note = random(['Fb2', 'G2']);
+    // Note velocity (volume, from 0 to 1)
+    let velocity = random();
+    // Time from now (in seconds)
+    let time = 0;
+    // Note duration (in seconds)
+    let dur = 1 / 6;
+
+    // Play the synthesizer note
+    synth.play(note, velocity, time, dur);
 }
 
     
